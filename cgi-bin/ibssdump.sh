eaafac009b0d2b7ca35da4bfc2c9e1ee0f0ba333
Geoffrey Kirk <geoffrey_kirk@trimble.com>
2016-08-20 07:52:54 -0600

    moved tmp directory to /tmp instead of the bluest one

--- cgi-bin/ibssdump.sh
+++ cgi-bin/ibssdump.sh
@@ -6,19 +6,9 @@ USER_ORG=`echo "$QUERY_STRING" | sed -n 's/^.*USER_ORG=\([^&]*\).*$/\1/p' | sed 
 PASS=`echo "$QUERY_STRING" | sed -n 's/^.*PASS=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
 HEADERS=`echo "$QUERY_STRING" | sed -n 's/^.*HEADERS=\([^&]*\).*$/\1/p' | sed "s/%20/ /g"`
 
-#export USER
-#export USER_ORG
-#export PASS
-
-#IP=sps851.com
-#PORT=2101
-#USER=status
-#USER_ORG=trimblehh
-#PASS=trimble
-
 
 echo -e "<h1>NTRIP Table information for $USER of $USER_ORG</h1>\n"
-curl -f -D headers_$$ -o ~/tmp/st_$$ --connect-timeout 10 -m 300  -H "Ntrip-Version: Ntrip/2.0" -H "User-Agent: NTRIP CURL_NTRIP_TEST/0.1" -u $USER:$PASS  http://$USER_ORG.ibss.trimbleos.com:2101/
+curl -f -D headers_$$ -o /tmp/st_$$ --connect-timeout 10 -m 300  -H "Ntrip-Version: Ntrip/2.0" -H "User-Agent: NTRIP CURL_NTRIP_TEST/0.1" -u $USER:$PASS  http://$USER_ORG.ibss.trimbleos.com:2101/
 RES=$?
 #echo "Result: $?"
 #stat ~/tmp/st_$$
@@ -26,7 +16,7 @@ RES=$?
 #cat ~/tmp/st_$$
 if [ $RES == 0 ]
 then
-    perl ibssdump.pl $USER $USER_ORG $PASS <~/tmp/st_$$
+    perl ibssdump.pl $USER $USER_ORG $PASS </tmp/st_$$
     echo "<p/>End of table"
 else
     if [ $RES == 22 ]
@@ -48,4 +38,4 @@ rm headers_$$
 echo "</body></html>"
 #echo "<pre>";
 #cat ~/tmp/st_$$
-rm ~/tmp/st_$$
+rm /tmp/st_$$
