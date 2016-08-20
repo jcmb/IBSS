2776b8f09d2bee10e6ceb86088e93dfb6bfe7c93
Geoffrey Kirk <geoffrey_kirk@trimble.com>
2016-08-20 07:14:58 -0600

    Basic install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+
+if ! type realpath &> /dev/null ; then
+   echo "Required realpath executable not found in $PATH" >&2
+   exit 2
+fi
+
+APP_DIR=`dirname $0`
+CURRENT_DIR=`realpath .`
+CURRENT_DIR=`dirname $CURRENT_DIR`
+echo $DIR $CURRENT_DIR
+
+if [ "$APP_DIR" != "$CURRENT_DIR" ] 
+then
+  echo "$0 needs to be run from the current directory"
+  exit 255
+fi
+
+cp -a WWW /var/www
+cp -a cgi-bin /usr/lib/cgi-bin
